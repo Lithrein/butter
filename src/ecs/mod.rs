5421ca930ff2a02b34b7bc8c72a6aa4f8c5957a2
Clément Sibille <claymeuns@protonmail.com>
2023-01-10 06:25:24 +0100

    Move Bitset into separate module

--- src/ecs/mod.rs
+++ src/ecs/mod.rs
@@ -1,4 +1,7 @@
+use crate::ecs::bitset::Bitset;
 use std::{alloc::Layout, any::TypeId, collections::HashMap, ptr::NonNull};
+
+mod bitset;
 
 pub struct Ecs {
     next_index: usize,
@@ -284,32 +287,6 @@ const fn max_size_for_align(align: usize) -> usize {
     isize::MAX as usize - (align - 1)
 }
 
-trait Bitset {
-    fn set_bit(&mut self, nth: usize);
-    fn unset_bit(&mut self, nth: usize);
-    fn bit(&self, nth: usize) -> bool;
-}
-
-impl Bitset for [u64; 1024] {
-    fn set_bit(&mut self, nth: usize) {
-        let word_index = nth >> 6;
-        let bit = nth & 63;
-        self[word_index] |= 1 << bit;
-    }
-
-    fn unset_bit(&mut self, nth: usize) {
-        let word_index = nth >> 6;
-        let bit = nth & 63;
-        self[word_index] &= !(1 << bit);
-    }
-
-    fn bit(&self, nth: usize) -> bool {
-        let word_index = nth >> 6;
-        let bit = nth & 63;
-        (self[word_index] >> bit) & 1 == 1
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -393,36 +370,5 @@ mod tests {
         health_component.0 = 5;
         let health_component = ecs.component::<Health>(player).unwrap();
         assert_eq!(health_component, &Health(5));
-    }
-
-    #[test]
-    fn bitset_set() {
-        let mut bitset = [0u64; 1024];
-        bitset.set_bit(5);
-        assert_eq!(bitset[0], 32);
-
-        bitset.set_bit(0);
-        assert_eq!(bitset[0], 33);
-
-        bitset.set_bit(64);
-        assert_eq!(bitset[1], 1);
-    }
-
-    #[test]
-    fn bitset_bit() {
-        let mut bitset = [0u64; 1024];
-        bitset.set_bit(5);
-        assert!(bitset.bit(5));
-        bitset.set_bit(65);
-        assert!(bitset.bit(65));
-    }
-
-    #[test]
-    fn bitset_unset() {
-        let mut bitset = [0u64; 1024];
-        bitset.set_bit(3);
-        assert!(bitset.bit(3));
-        bitset.unset_bit(3);
-        assert!(!bitset.bit(3));
     }
 }
